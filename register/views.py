6adf421797cd6b0fc4313bdde76f16d72d6f5263
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2018-11-05 19:45:35 -0300

    Created homepage

--- register/views.py
+++ register/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+#Homepage
+def home(request):
+    return render(request, 'register/home.html')
