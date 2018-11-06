6abee62b392a2e595324e7cfc880c64c7d9656c1
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2018-11-05 21:04:35 -0300

    Created cliente and telefone_cliente models

--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import cliente, telefone_cliente
 
 # Register your models here.
+admin.site.register(cliente)
+admin.site.register(telefone_cliente)
--- register/models.py
+++ register/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+
+#User model
+class cliente(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=15)
+    sobrenome = models.CharField(max_length=15)
+    dt_nascimento = models.DateTimeField()
+    nacionalidade = models.CharField(max_length=15)
+
+    #class Meta:
+     #   verbose_name_plural = 'Clientes'
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.nome
+
+class telefone_cliente(models.Model):
+
+    id = models.ForeignKey(cliente, on_delete=models.CASCADE)
+    telefone = models.CharField(max_length=12, primary_key=True)
+
+    class Meta:
+        verbose_name_plural = 'Telefones Clientes'
+
+    def __str__(self):
+        return str(self.id) + ' - ' + str(self.telefone)
